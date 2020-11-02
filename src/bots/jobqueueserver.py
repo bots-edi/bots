061f2e25e5b2f9823d271e246c3768080905be45
rmathijn <rm.andela@gmail.com>
2020-11-02 16:41:32 +0100

    Python3 fixes for jobqueueserver

--- src/bots/jobqueueserver.py
+++ src/bots/jobqueueserver.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import sys
 import os
-import xmlrpclib
-from SimpleXMLRPCServer import SimpleXMLRPCServer
+import xmlrpc.client as xmlrpclib
+from xmlrpc.server import SimpleXMLRPCServer
 import time
 import datetime
 import subprocess
 import threading
-import botsinit
-import botslib
-import botsglobal
+import bots.botsinit as botsinit
+import bots.botslib as botslib
+import bots.botsglobal as botsglobal
 
 
 #-------------------------------------------------------------------------------
@@ -113,15 +113,15 @@ def start():
         if arg.startswith('-c'):
             configdir = arg[2:]
             if not configdir:
-                print 'Error: configuration directory indicated, but no directory name.'
+                print('Error: configuration directory indicated, but no directory name.')
                 sys.exit(1)
         else:
-            print usage
+            print(usage)
             sys.exit(0)
     #***end handling command line arguments**************************
     botsinit.generalinit(configdir)     #find locating of bots, configfiles, init paths etc.
     if not botsglobal.ini.getboolean('jobqueue','enabled',False):
-        print 'Error: bots jobqueue cannot start; not enabled in %s/bots.ini' % configdir
+        print('Error: bots jobqueue cannot start; not enabled in %s/bots.ini' % configdir)
         sys.exit(1)
     process_name = 'jobqueue'
     logger = botsinit.initserverlogging(process_name)
